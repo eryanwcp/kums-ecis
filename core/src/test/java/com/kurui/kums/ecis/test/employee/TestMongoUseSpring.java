6a6cd6b710f34a6aefb878b32c107c03b0cb7469
Administrator <Administrator@home-t39v8elt3m.mshome.net>
2012-08-26 06:44:31 +0800

    edit config

--- core/src/test/java/com/kurui/kums/ecis/test/employee/TestMongoUseSpring.java
+++ core/src/test/java/com/kurui/kums/ecis/test/employee/TestMongoUseSpring.java
@@ -13,36 +13,36 @@ import org.springframework.data.document.mongodb.query.Update;
 
 public class TestMongoUseSpring extends TestCase {
 
-	public static void testEmployee() {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext(
-				"com/kurui/kums/ecis/test/employee/mongodb-config.xml");
-		MongoOperations mongoOperation = (MongoOperations) ctx
-				.getBean("mongoTemplate");
-
-		Employee employee = new Employee("100", "firstName", "lastName", 23);
-		// save
-		mongoOperation.save("employees", employee);
-		// find
-		Employee savedEmployee = mongoOperation.findOne("employees", new Query(
-				Criteria.where("id").is("100")), Employee.class);
-		System.out.println("Saved Employee: " + savedEmployee);
-		// update
-		mongoOperation.updateFirst("employees",
-				new Query(Criteria.where("firstname").is("firstName")),
-				Update.update("lastname", "new lastName"));
-		// find
-		Employee updatedEmployee = mongoOperation.findOne("employees",
-				new Query(Criteria.where("id").is("100")), Employee.class);
-		System.out.println("Updated Employee: " + updatedEmployee);
-
-		// delete
-		// mongoOperation.remove("employees",
-		// new Query(Criteria.where("id").is("100")), Employee.class);
-
-		// List
-		List<Employee> listEmployee = mongoOperation.getCollection("employees",
-				Employee.class);
-		System.out.println("size of employees = " + listEmployee.size());
-
-	}
+//	public static void testEmployee() {
+//		ApplicationContext ctx = new ClassPathXmlApplicationContext(
+//				"com/kurui/kums/ecis/test/mongodb-config.xml");
+//		MongoOperations mongoOperation = (MongoOperations) ctx
+//				.getBean("mongoTemplate");
+//
+//		Employee employee = new Employee("100", "firstName", "lastName", 23);
+//		// save
+//		mongoOperation.save("employees", employee);
+//		// find
+//		Employee savedEmployee = mongoOperation.findOne("employees", new Query(
+//				Criteria.where("id").is("100")), Employee.class);
+//		System.out.println("Saved Employee: " + savedEmployee);
+//		// update
+//		mongoOperation.updateFirst("employees",
+//				new Query(Criteria.where("firstname").is("firstName")),
+//				Update.update("lastname", "new lastName"));
+//		// find
+//		Employee updatedEmployee = mongoOperation.findOne("employees",
+//				new Query(Criteria.where("id").is("100")), Employee.class);
+//		System.out.println("Updated Employee: " + updatedEmployee);
+//
+////		 delete
+//		 mongoOperation.remove("employees",
+//		 new Query(Criteria.where("id").is("100")), Employee.class);
+//
+//		// List
+//		List<Employee> listEmployee = mongoOperation.getCollection("employees",
+//				Employee.class);
+//		System.out.println("size of employees = " + listEmployee.size());
+//
+//	}
 }
